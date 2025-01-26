77ad1e2c38fbd205407eea48a4ccb56a8a2e18e3
Sergey Kostyaev <sskostyaev@gmail.com>
2025-01-27 03:45:18 +0700

    Bump version

--- ellama.el
+++ ellama.el
@@ -6,7 +6,7 @@
 ;; URL: http://github.com/s-kostyaev/ellama
 ;; Keywords: help local tools
 ;; Package-Requires: ((emacs "28.1") (llm "0.22.0") (spinner "1.7.4") (transient "0.7") (compat "29.1"))
-;; Version: 0.13.3
+;; Version: 0.13.4
 ;; SPDX-License-Identifier: GPL-3.0-or-later
 ;; Created: 8th Oct 2023
 
